b423b0dba73a267a1059b08483601198c9a3c7be
yasinguler007 <yasinguler58@outlook.com>
2020-11-03 09:57:32 +0300

    kusaködemleri entity eklendi

---
+++ DevFramework.Northwind.Entities/Concrete/KusakOdemeleri.cs
@@ -0,0 +1,17 @@
+﻿using DevFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Entities.Concrete
+{
+    public class KusakOdemeleri:IEntity
+    {
+        public virtual int Id { get; set; }
+        public virtual int KusakId { get; set; }
+        public virtual int SporcuId { get; set; }
+        public virtual string OdemeTarihi { get; set; }
+    }
+}
