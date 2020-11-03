0a103fb26cf4080525c4f6c5b098dd0cfdcb7bad
yasinguler007 <yasinguler58@outlook.com>
2020-11-03 10:31:45 +0300

    Dataaccess için kuşak ödemleri maplendi

---
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/Mappings/KusakOdemeleriMap.cs
@@ -0,0 +1,24 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class KusakOdemeleriMap : EntityTypeConfiguration<KusakOdemeleri>
+    {
+        public KusakOdemeleriMap()
+        {
+            ToTable(@"KusakOdemeleris", @"dbo");
+            HasKey(x => x.Id);
+
+            Property(x => x.KusakId).HasColumnName("KusakId");
+            Property(x => x.SporcuId).HasColumnName("SporcuId");
+            Property(x => x.OdemeTarihi).HasColumnName("OdemeTarihi");
+  
+        }
+    }
+}
