d9f01fc6783e7b5f5bc16b47750414b363a134bf
yasinguler007 <yasinguler58@outlook.com>
2020-11-03 10:47:43 +0300

    Business kısmının abstractına kusaködemeleri interface yazıldı

---
+++ DevFramework.Northwind.Business/Abstract/IKusakOdemeleriService.cs
@@ -0,0 +1,25 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.Abstract
+{
+    [ServiceContract]
+    public interface IKusakOdemeleriService
+    {
+        [OperationContract]
+        List<KusakOdemeleri> GetAll();
+        [OperationContract]
+        KusakOdemeleri GetById(int id);
+        [OperationContract]
+        KusakOdemeleri Add(KusakOdemeleri kusakOdemeleri);
+        [OperationContract]
+        KusakOdemeleri Update(KusakOdemeleri kusakOdemeleri);
+        [OperationContract]
+        void Delete(int Id);
+    }
+}
