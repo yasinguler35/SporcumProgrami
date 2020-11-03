a8f74cf9a884401f0146950dff6d66f259e947e4
yasinguler007 <yasinguler58@outlook.com>
2020-11-03 17:19:16 +0300

    kusaklar scripte eklendi

--- DevFramework.Northwind.MvcWebUI/Scripts/datatablelistler.js
+++ DevFramework.Northwind.MvcWebUI/Scripts/datatablelistler.js
@@ -98,7 +98,7 @@ $(document).ready(function () {
 
 
 
-//sporcu kategırileri listele sil düzenle 
+//sporcu kategorileri listele sil düzenle 
 $(document).ready(function () {
     var oTable = $('#dataTables-sporcukategorilist').DataTable({
         responsive: true,
@@ -312,6 +312,84 @@ $(document).ready(function () {
                       title: 'Sporcu Aylik Düzenleme Formu',
                       height: 550,
                       width: 900,
+                      close: function () {
+                          $dialog.dialog('destroy').remove();
+                      }
+                  })
+
+        $('.popupWindow').on('submit', '#popupForm', function (e) {
+            var url = $('#popupForm')[0].action;
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $('#popupForm').serialize(),
+                success: function (data) {
+                    if (data.status) {
+                        $dialog.dialog('close');
+                        oTable.ajax.reload();
+                    }
+                }
+            })
+
+            e.preventDefault();
+        })
+
+        $dialog.dialog('open');
+    }
+
+});
+
+
+//kusaklar listele sil düzenle 
+$(document).ready(function () {
+    var oTable = $('#dataTables-kusaklarlist').DataTable({
+        responsive: true,
+        "ajax": {
+            "url": '/Kusaklar/KusaklarGetir',
+            "type": "get",
+            "datatype": "json"
+        },
+        "columns": [
+            //{ "data": "Id", "autoWidth": true },
+            { "data": "KusakAdi", "autoWidth": true },
+            {
+                "data": "Id", "width": "50px", "render": function (data) {
+                    return '<a class="popup btn btn-success" href="/Kusaklar/KusaklarKayit/' + data + '">Düzenle</a>';
+                }
+            },
+            {
+                "data": "Id", "width": "50px", "render": function (data) {
+                    return '<a class="popup btn btn-danger"  href="/Kusaklar/KusaklarSil/' + data + '">Sil</a>';
+                }
+            },
+        ]
+    })
+
+
+    $('.tablecontainerkusaklar').on('click', 'a.popup', function (e) {
+        e.preventDefault();
+        OpenPopup($(this).attr('href'));
+    })
+
+    function OpenPopup(pageUrl) {
+        var $pageContent = $('<div/>');
+        $pageContent.load(pageUrl, function () {
+            $('#popupForm', $pageContent).removeData('validator');
+            $('#popupForm', $pageContent).removeData('unobtrusiveValidation');
+            $.validator.unobtrusive.parse('form');
+
+        });
+
+        $dialog = $('<div class="popupWindow" style="overflow:auto"></div>')
+                  .html($pageContent)
+                  .dialog({
+                      draggable: false,
+                      autoOpen: false,
+                      resizable: false,
+                      model: true,
+                      title: 'Kuşak Düzenleme Formu',
+                      height: 200,
+                      width: 300,
                       close: function () {
                           $dialog.dialog('destroy').remove();
                       }
