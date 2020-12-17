02408a3d2dc20947eef0dbc2e0e54bfcbe2e5689
yasinguler007 <yasinguler58@outlook.com>
2020-12-17 16:43:06 +0300

    business katmanında tesisfaturam validator yazıldı

---
+++ DevFramework.Northwind.Business/Concrete/Managers/TesisFaturamManager.cs
@@ -0,0 +1,47 @@
+﻿using DevFramework.Northwind.Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Northwind.Entities.Concrete;
+using DevFramework.Northwind.DataAccess.Abstract;
+using AutoMapper;
+
+namespace DevFramework.Northwind.Business.Concrete.Managers
+{
+    public class TesisFaturamManager : ITesisFaturamService
+    {
+        private ITesisFaturamDal _tesisFaturamDal;
+        private readonly IMapper _mapper;
+        public TesisFaturamManager(ITesisFaturamDal tesisFaturamDal, IMapper mapper)
+        {
+            _mapper = mapper;
+            _tesisFaturamDal = tesisFaturamDal;
+        }
+        public TesisFaturam Add(TesisFaturam tesisFaturam)
+        {
+            return _tesisFaturamDal.Add(tesisFaturam);
+        }
+
+        public void Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TesisFaturam> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TesisFaturam GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TesisFaturam Update(TesisFaturam tesisFaturam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DevFramework.Northwind.Business/ValidationRules/FluentValidation/TesisFaturamValidatior.cs
@@ -0,0 +1,18 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.ValidationRules.FluentValidation
+{
+    public class TesisFaturamValidatior : AbstractValidator<TesisFaturam>
+    {
+        public TesisFaturamValidatior()
+        {
+            RuleFor(p => p.TesisFaturaTuru).NotEmpty();
+        }
+    }
+}
