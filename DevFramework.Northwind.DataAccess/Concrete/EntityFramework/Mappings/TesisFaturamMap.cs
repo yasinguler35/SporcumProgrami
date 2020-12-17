9d0822a1df8142dcd877ce16ae5b93537f1cf087
yasinguler007 <yasinguler58@outlook.com>
2020-12-17 15:49:17 +0300

    entityframework testifaturam map eklendi

---
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/Mappings/TesisFaturamMap.cs
@@ -0,0 +1,24 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class TesisFaturamMap : EntityTypeConfiguration<TesisFaturam>
+    {
+        public TesisFaturamMap()
+        {
+            ToTable(@"TesisFaturams", @"dbo");
+            HasKey(x => x.Id);
+
+            Property(x => x.IslemTarihi).HasColumnName("IslemTarihi");
+            Property(x => x.SonOdeme).HasColumnName("SonOdeme");
+            Property(x => x.TesisFaturaTuru).HasColumnName("TesisFaturaTuru");
+            Property(x => x.TesisFaturaTutari).HasColumnName("TesisFaturaTutari");
+        }
+    }
+}
