23c555a213d3022ae9ddc7d265475ab17481db6c
yasinguler007 <yasinguler58@outlook.com>
2020-12-17 16:21:45 +0300

    testifaturam business katmanında abstratrct interface ' i yazıldı

---
+++ DevFramework.Northwind.Business/Abstract/ITesisFaturamService.cs
@@ -0,0 +1,25 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.Abstract
+{
+    [ServiceContract]
+    public interface ITesisFaturamService
+    {
+        [OperationContract]
+        List<TesisFaturam> GetAll();
+        [OperationContract]
+        TesisFaturam GetById(int id);
+        [OperationContract]
+        TesisFaturam Add(TesisFaturam tesisFaturam);
+        [OperationContract]
+        TesisFaturam Update(TesisFaturam tesisFaturam);
+        [OperationContract]
+        void Delete(int Id);
+    }
+}
