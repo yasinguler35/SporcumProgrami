67944c1bded95700466d70581615393944763c80
yasinguler007 <yasinguler58@outlook.com>
2021-03-23 14:26:37 +0300

    tesisfaturam  getlist model eklendi

---
+++ DevFramework.Northwind.MvcWebUI/Controllers/TesisFaturamController.cs
@@ -0,0 +1,27 @@
+﻿using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevFramework.Northwind.MvcWebUI.Controllers
+{
+    public class TesisFaturamController : Controller
+    {
+        private ITesisFaturamService _tesisFaturamService;
+        public TesisFaturamController(ITesisFaturamService tesisFaturamService)
+        {
+            tesisFaturamService = _tesisFaturamService;
+        }
+        public ActionResult TesisFaturamGetir()
+        {
+            var model = new TesisFaturamListViewModel
+            {
+                tesisFaturams = _tesisFaturamService.GetAll()
+            };
+            return Json(new { data = model.tesisFaturams }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ DevFramework.Northwind.MvcWebUI/Models/TesisFaturamListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevFramework.Northwind.MvcWebUI.Models
+{
+    public class TesisFaturamListViewModel
+    {
+        public List<TesisFaturam> tesisFaturams { get; set; }
+        public TesisFaturam tesisFaturamGet { get; set; }
+    }
+}
