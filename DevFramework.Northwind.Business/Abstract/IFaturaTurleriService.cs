401b020ca278944881cc56fd9e1f3f2c5f633035
yasinguler007 <yasinguler58@outlook.com>
2021-03-29 14:24:46 +0300

    business katamnında faturaturleriservice interface i oluşturuldu

---
+++ DevFramework.Northwind.Business/Abstract/IFaturaTurleriService.cs
@@ -0,0 +1,25 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.Abstract
+{
+    [ServiceContract]
+    public interface IFaturaTurleriService
+    {
+        [OperationContract]
+        List<FaturaTurleri> GetAll();
+        [OperationContract]
+        FaturaTurleri GetById(int id);
+        [OperationContract]
+        FaturaTurleri Add(FaturaTurleri faturaTurleri);
+        [OperationContract]
+        FaturaTurleri Update(FaturaTurleri faturaTurleri);
+        [OperationContract]
+        void Delete(int Id);
+    }
+}
