2005f625068b75cee8bce40400109e2f373ed3de
yasinguler007 <yasinguler58@outlook.com>
2021-04-08 13:02:53 +0300

    Fatura turleri controlleri yazıldı

---
+++ DevFramework.Northwind.MvcWebUI/Controllers/FaturaTurleriController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevFramework.Northwind.MvcWebUI.Controllers
+{
+    public class FaturaTurleriController : Controller
+    {
+        // GET: FaturaTurleri
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
