a16f2d5c40f3256e72f5e893820b17335adb8a02
yasinguler007 <yasinguler58@outlook.com>
2021-04-09 11:59:32 +0300

    controller ve view fatura türü listeleme yazıldı

--- DevFramework.Northwind.MvcWebUI/Controllers/FaturaTurleriController.cs
+++ DevFramework.Northwind.MvcWebUI/Controllers/FaturaTurleriController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.MvcWebUI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,29 @@ namespace DevFramework.Northwind.MvcWebUI.Controllers
 {
     public class FaturaTurleriController : Controller
     {
+        private IFaturaTurleriService _faturaTurleriService;
+        public FaturaTurleriController(IFaturaTurleriService faturaTurleriService)
+        {
+            _faturaTurleriService = faturaTurleriService;
+        }
         // GET: FaturaTurleri
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult FaturaTurleriList()
+        {
+            return View();
+
+        }
+        public ActionResult FaturaTurleriGetir()
+        {
+            var model = new FaturaTurleriListViewModel
+            {
+                faturaTurleris = _faturaTurleriService.GetAll()
+            };
+            return Json(new { data = model.faturaTurleris }, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
