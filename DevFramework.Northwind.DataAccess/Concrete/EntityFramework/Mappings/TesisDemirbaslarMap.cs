cb65eb5cde8c39b735acac46e63edd5648885fdf
yasinguler007 <yasinguler58@outlook.com>
2021-04-13 14:28:07 +0300

    TesisDemirbaslarMap mapleme işlemi eklendi

---
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/Mappings/TesisDemirbaslarMap.cs
@@ -0,0 +1,24 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class TesisDemirbaslarMap : EntityTypeConfiguration<TesisDemirbaslar>
+    {
+        public TesisDemirbaslarMap()
+        {
+            ToTable(@"TesisDemirbaslars", @"dbo");
+            HasKey(x => x.Id);
+
+            Property(x => x.DemirBasAdi).HasColumnName("DemirBasAdi");
+            Property(x => x.AlimFiyati).HasColumnName("AlimFiyati");
+            Property(x => x.KullanimAmaci).HasColumnName("KullanimAmaci");
+            Property(x => x.KullanimYeri).HasColumnName("KullanimYeri");
+        }
+    }
+}
