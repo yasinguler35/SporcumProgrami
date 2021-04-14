6b87a3284bfb11fb875c281466aa0e14da922929
yasinguler007 <yasinguler58@outlook.com>
2021-04-14 12:41:26 +0300

    tesisdemirbaslar business abstact interface yazıldı

---
+++ DevFramework.Northwind.Business/Abstract/ITesisDemirbaslarService.cs
@@ -0,0 +1,25 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.Abstract
+{
+    [ServiceContract]
+    public interface ITesisDemirbaslarService
+    {
+        [OperationContract]
+        List<TesisDemirbaslar> GetAll();
+        [OperationContract]
+        TesisDemirbaslar GetById(int id);
+        [OperationContract]
+        TesisDemirbaslar Add(TesisDemirbaslar tesisDemirbaslar);
+        [OperationContract]
+        TesisDemirbaslar Update(TesisDemirbaslar tesisDemirbaslar);
+        [OperationContract]
+        void Delete(int Id);
+    }
+}
