6c27a268ca504fbd155e55b427479f7cd38867c9
yasinguler007 <yasinguler58@outlook.com>
2021-04-14 15:47:46 +0300

    tesisdemirbaslar getlist controlu ve modeli yazıldı

---
+++ DevFramework.Northwind.MvcWebUI/Controllers/TesisDemirbaslarController.cs
@@ -0,0 +1,38 @@
+﻿using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevFramework.Northwind.MvcWebUI.Controllers
+{
+    public class TesisDemirbaslarController : Controller
+    {
+        private ITesisDemirbaslarService _tesisDemirbaslar;
+        public TesisDemirbaslarController(ITesisDemirbaslarService tesisDemirbaslar)
+        {
+            _tesisDemirbaslar = tesisDemirbaslar;
+        }
+        // GET: TesisDemirbaslar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TesisDemirbaslarList()
+        {
+            return View();
+
+        }
+        public ActionResult TesisDemirbaslarGetir()
+        {
+            var model = new TesisDemirbaslarListViewModel
+            {
+                tesisDemirbaslars = _tesisDemirbaslar.GetAll()
+            };
+            return Json(new { data = model.tesisDemirbaslars }, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ DevFramework.Northwind.MvcWebUI/Models/TesisDemirbaslarListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevFramework.Northwind.MvcWebUI.Models
+{
+    public class TesisDemirbaslarListViewModel
+    {
+        public List<TesisDemirbaslar> tesisDemirbaslars { get; set; }
+        public TesisDemirbaslar tesisDemirbaslarGet { get; set; }
+    }
+}
